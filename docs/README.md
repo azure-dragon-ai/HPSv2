4da1f8da959ecf3a00a2fd01a825f1596101da87
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-05-06 11:04:25 +0800

    update

--- docs/README.md
+++ docs/README.md
@@ -99,6 +99,12 @@ pip install -r requirements.txt
 ```
 
 ```shell
+cd /data/ComfyUI
+source hjh/bin/activate
+export HF_ENDPOINT=https://hf-mirror.com
+export HPS_ROOT=/data/models/Score/HPSv2Models
+python main.py --cuda-device=0 --highvram --force-fp16 --listen=0.0.0.0 --port=20003
+
 python ./main.py --listen 0.0.0.0  --port=8188
 http://localhost:8188
 
--- docs/start.sh
+++ docs/start.sh
@@ -1,3 +1,6 @@
+#! /bin/bash
+cd /data/ComfyUI
+source hjh/bin/activate
 export HF_ENDPOINT=https://hf-mirror.com
 export HPS_ROOT=/data/models/Score/HPSv2Models
-python main.py --cuda-device=0 --highvram --force-fp16 --listen=[2408:8207:60ad:6cb0::404] --port=8188 --output-directory=/data/Web/Resource/ResDev/ComfyUI
+python main.py --cuda-device=0 --highvram --force-fp16 --listen=0.0.0.0 --port=20003
